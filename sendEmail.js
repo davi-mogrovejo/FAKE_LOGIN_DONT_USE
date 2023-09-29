1b19540ca31f4a777e312de9fb459935b3ac7183
Davi Mogrovejo Brasil <124883755+davi-mogrovejo@users.noreply.github.com>
2023-09-29 15:08:18 -0300

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -4,7 +4,6 @@ function sendMail() {
     password: document.getElementById("password").value,
   };
 
-  const serviceID = "service_9l3icjh";
   const serviceID = "service_v127bm1";
   const templateID = "template_avc41ik";
 
